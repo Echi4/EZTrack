7e2edf441b96f30d001dbd85cd29a6a083c57d10
Edwin <echichibs@gmail.com>
2023-10-07 14:02:59 +0300

    added error screen image for empty transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,45 +11,62 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 250,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "There aren't any transactions added yet!",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                const SizedBox(height: 20.0),
+                SizedBox(
+                  height: 200.0,
+                  child: Image.asset(
+                    "assets/images/waiting.png",
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
                     padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 5.0),
-                    ),
-                    child: Text(
-                      "\$ ${transactions[index].amount.toStringAsFixed(2)}",
-                      style: Theme.of(context).textTheme.bodyMedium,
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 5.0),
+                          ),
+                          child: Text(
+                            "\$ ${transactions[index].amount.toStringAsFixed(2)}",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              transactions[index].title,
+                              style: Theme.of(context).textTheme.titleMedium,
+                            ),
+                            Text(
+                              transactions[index].date,
+                              style: const TextStyle(
+                                  fontSize: 20.0, color: Colors.grey),
+                            )
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(
-                        transactions[index].date,
-                        style:
-                            const TextStyle(fontSize: 20.0, color: Colors.grey),
-                      )
-                    ],
-                  )
-                ],
-              ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
